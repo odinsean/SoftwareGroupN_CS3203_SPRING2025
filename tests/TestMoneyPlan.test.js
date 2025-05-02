2d33b26f660f9b21d53433b91e2cfc5d51abf84b
Sean Ropp <86846481+odinsean@users.noreply.github.com>
2025-05-02 09:17:06 -0500

    Update TestMoneyPlan.test.js added an instruction plus updated for the new exchange rates

--- tests/TestMoneyPlan.test.js
+++ tests/TestMoneyPlan.test.js
@@ -1,3 +1,4 @@
+// npx jest TestMoneyPlan.test.js to run this test
 const Money = require('../js/MoneyPlan');
 const { describe, test, expect } = require('@jest/globals');
 
@@ -18,10 +19,9 @@ describe('Money', () => {
         let user4 = new Money("Tom",-10000000000000000000000000000, "USD", "EURO");  // edge case
         expect(user4.takeUserPreference()).toBe("User has invaild amount of money!");
     });
-
     test('createMoneyPlan should convert currency correctly', () => {
         let user1 = new Money("Alice", 100, "USD", "EURO"); // correct case
-        expect(user1.createMoneyPlan()).toBeCloseTo(92.00);
+        expect(user1.createMoneyPlan()).toBeCloseTo(88.00);
     });
     test('createMoneyPlan should return error for invalid currency conversion', () => {
         let user2 = new Money("Jerry", 100, "USD", "INVALID"); // incorrect case
@@ -33,27 +33,24 @@ describe('Money', () => {
     });
     test('createMoneyPlan should handle extremely large positive amounts', () => { // bound case
         let user4 = new Money("Harry", 100000000000000000000000000000, "USD", "EURO");
-        expect(user4.createMoneyPlan()).toBe(9.2e+28);
+        expect(user4.createMoneyPlan()).toBe(8.8e+28);
     });
-    
     test('createMoneyPlan should return error for extremely large negative amounts', () => { // edge case
         let user5 = new Money("Tom", -10000000000000000000000000000, "USD", "EURO");
         expect(user5.createMoneyPlan()).toBe("User has invaild amount of money!");
     });
-
     test('displayTotalCost should return formatted total', () => { // correct case
         let user1 = new Money("Alice", 150.75, "USD", "USD");
         expect(user1.displayTotalCost()).toBe("Alice has 150.75 USD");
     });
-
     test('displayTotalCost should return error for invalid money amount', () => { // incorrect case
         let user2 = new Money("Bob", "Invalid", "USD", "USD");
         expect(user2.displayTotalCost()).toBe("Invalid total cost!");
     });
-
-    test('displayTotalCost should return error for invalid money amount', () => { // bound case
+    test('displayTotalCost should return formatted total', () => { // bound case
         let user3 = new Money("Jerry",100000000000000000000000000000, "USD", "EURO");
-        expect(user3.displayTotalCost()).toBe("Jerry has 1e+29 USD");
+        user3.createMoneyPlan();
+        expect(user3.displayTotalCost()).toBe("Jerry has 8.8e+28 EURO");
     });
     test('displayTotalCost should return error for invalid money amount', () => { // edge case
         let user4 = new Money("Jerry",-100000000000000000000000000000, "USD", "EURO");
